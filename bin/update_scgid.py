cbc06677210efefbf2281e46d10249746a60d7c3
kevinamses <amsesk@umich.edu>
2018-09-10 19:50:56 -0400

    add confirmation prior to update

--- bin/update_scgid.py
+++ bin/update_scgid.py
@@ -1,14 +1,26 @@
 import subprocess
 import os
 import inspect
+import sys
 
 ## some variables
 scgid_bin = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 scgid_home = os.path.dirname(scgid_bin)
 
-## navigate to repo
+## Confirm
+while True:
+    entry = raw_input("You are about to overwrite the scgid source code with updates from GitHub. Continue? [y/n]")
+    entry = entry.lower()
+    if entry not in ['y','n']:
+        continue
+    else:
+        if entry == 'y':
+            break
+        else:
+            sys.exit()
+
+## navigate to local repo
 os.chdir(scgid_home)
-print os.getcwd()
 ## save settings for hard reset from origin
 os.rename(os.path.join(scgid_bin,'settings.py'), os.path.join(scgid_bin,'settings.py.sav'))
 
