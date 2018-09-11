1338fd3e4a331a8f4c6597133693c027ec1441de
amsesk <amsesk@umich.edu>
2018-09-11 13:25:27 -0400

    Update settings.py

--- bin/settings.py
+++ bin/settings.py
@@ -3,6 +3,6 @@ path_to_taxdb=""
 taxonomy_all_tab=""
 esom_path=""
 clams_path=""
-AUGUSTS_CONFIG_PATH=""
+AUGUSTS_CONFIG_PATH="" ## You probably don't have to set this
 spdb_version=""
 path_to_Rscript=""
