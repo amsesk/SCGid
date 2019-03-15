615ab5cbcad0c951ecc6308a378b4e0e22053f42
Kevin Amses <kevinamses@Kevins-MacBook-Pro-5.local>
2019-03-14 19:12:19 -0700

    switch to ggplot

--- bin/gc_cov.plot.R
+++ bin/gc_cov.plot.R
@@ -1,52 +1,181 @@
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+
+getTaxLevel <- function (row) {
+  spl<-as.list(strsplit(row, ", ")[[1]])
+  return(spl[2])
+}
+
+### Test case
+#args = c("~/Documents/Flux_Downlaods/dissertation/rhop_neo/scgid/RHalf-1f_scgid_output/blob/RHalf-1f_info_table.tsv",
+#         "~/Documents/Flux_Downlaods/dissertation/rhop_neo/scgid/RHalf-1f_scgid_output/blob/RHalf-1f_unclassified_info_table.tsv",
+#         "(0.41,0.75","(0.0468227,8.0)")
+### Read in data from <scgid gc-cov>
+
+#tigInfo<-read.table(args[1],sep="\t",
+#                    col.names = c("contigid","plen","coverage","gc","pid","hitsp","hitlin","evalue","parse_lin"))
+#unclass<-read.table(args[2],sep="\t", col.names=c("contigid","coverage","gc","parse_lin"))
+#unclass$color = "black"
+#unclass$coverage = log(unclass$coverage)
+
+#gc_window<-args[3]
+#cov_window<-args[4]
 
 args<-commandArgs(trailingOnly = TRUE)
 
 tigInfo<-read.table(args[1],sep="\t")
 unclass<-read.table(args[2],sep="\t")
-colnames(tigInfo)<-c("contig","prot_len","coverage","gc","pid","sp_os","lineage","evalue","parse_lineage")
-colnames(unclass)<-c("contig","coverage","gc","taxonomy")
+colnames(tigInfo)<-c("contigid","plen","coverage","gc","pid","hitsp","hitlin","evalue","parse_lin")
+colnames(unclass)<-c("contigid","coverage","gc","parse_lin")
 gc_window<-args[3]
 cov_window<-args[4]
 output<-args[5]
 
+lin<-as.character(tigInfo$hitlin)
+lin<-sub("\\[","",lin)
+lin<-sub("\\]","",lin)
+linapp<-unlist(lapply(lin, getTaxLevel))
+tigInfo$hitlin_level<-linapp
+
 gc_window = gsub("[()]","",gc_window)
 cov_window = gsub("[()]","",cov_window)
-
-gc_window = strsplit(gc_window,split=',')
-cov_window = strsplit(cov_window,split=',')
-
-choices = c(rgb(0,0,1,alpha=0.6),rgb(0.54,0,0,alpha=0.6))
-tigInfo$color = "black"
-try(tigInfo[tigInfo$evalue == 0.0,]$evalue<-2.225074e-308, silent = TRUE)
-tigInfo$log_evalue<-log(tigInfo$evalue)
-try(tigInfo[tigInfo$parse_lineage == "target",]$color<-choices[1], silent = TRUE)
-try(tigInfo[tigInfo$parse_lineage == "nontarget",]$color<-choices[2], silent = TRUE)
-unclass$color<-rgb(0,0,0,alpha=0.45)
-rect_color = rgb(0,1,0,alpha=0.5)
-xlim = c(0,1050)
+gc_window = as.numeric(unlist(strsplit(gc_window,split=',')))
+cov_window = log(as.numeric(unlist(strsplit(cov_window,split=','))))
+window = as.data.frame(cbind(cov_window, gc_window))
 
 
-leg<-matrix(nrow=4,ncol=3)
-leg[,1]<-c("Target","Non-target","Unclassified","Window")
-leg[,2]<-c(choices,rgb(0,0,0,alpha=0.45),"black")
-leg[,3]<-c(19,19,19,22)
+imp<-tigInfo %>% group_by(hitlin_level) %>% summarise (n = n()) %>% 
+  filter(n>dim(tigInfo)[1]/1000)
+
+try(tigInfo[!tigInfo$hitlin_level %in% imp$hitlin_level,]$hitlin_level <- "Other", silent = TRUE)
+try(tigInfo[tigInfo$evalue == 0,]$evalue<-2.225074e-308, silent = TRUE)
+tigInfo<-tigInfo %>% mutate(log_evalue = log(evalue))
+
+
+plt<-tigInfo %>% select(contigid,coverage,gc,parse_lin,log_evalue,hitlin_level)
+plt$coverage<-log(plt$coverage)
+#plt<-melt(plt, id.vars=c("contigid","coverage","gc"))
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+tntPalette <- c("darkred","blue")
+
+unclass$color = "black"
+unclass$coverage = log(unclass$coverage)
 
 pdf(output)
-plot(tigInfo$coverage,tigInfo$gc,cex=0.25,col=tigInfo$color,xlab="Coverage",ylab="GC content",pch=19,xlim=xlim)
-rect(as.numeric(cov_window[[1]][1]),as.numeric(gc_window[[1]][1]),
-     as.numeric(cov_window[[1]][2]),as.numeric(gc_window[[1]][2]),col=rect_color)
-legend("topright",leg[,1],col=leg[,2],pch=c(19,19,19,22),pt.bg=rect_color,pt.cex=2,cex=0.9)
 
-plot(tigInfo$coverage,tigInfo$gc,cex=0.25,col=tigInfo$color,xlab="Coverage",ylab="GC content",pch=19,xlim=xlim)
-points(unclass$coverage,unclass$gc,cex=0.25,col=unclass$color,pch=19)
-rect(as.numeric(cov_window[[1]][1]),as.numeric(gc_window[[1]][1]),
-     as.numeric(cov_window[[1]][2]),as.numeric(gc_window[[1]][2]),col=rect_color)
-legend("topright",leg[,1],col=leg[,2],pch=c(19,19,19,22),pt.bg=rect_color,pt.cex=2,cex=0.9)
+### Just annotated points
+ggplot(plt, aes(coverage, gc, color=hitlin_level, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  #scale_colour_manual(values=cbPalette) +
+  scale_color_brewer(type="qual", palette = "Dark2") +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+    ) +
+  #geom_point(data=unclass, aes(log(coverage),gc, size=0.1), alpha=0.10, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  #geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+                             #xmax=cov_window[2], ymax=gc_window[2]), fill = "green", color="black", alpha=0.2, inherit.aes = FALSE) +
+  theme_bw()
 
-plot(tigInfo$coverage,tigInfo$gc,cex=0.25,col=tigInfo$color,xlab="Coverage",ylab="GC content",pch=19,xlim=xlim)
-legend("topright",leg[,1],col=leg[,2],pch=c(19,19,19,22),pt.bg=rect_color,pt.cex=2,cex=0.9)
+## annotated points and unclassified
+ggplot(plt, aes(coverage, gc, color=hitlin_level, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  scale_colour_manual(values=cbPalette) +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+  ) +
+  geom_point(data=unclass, aes(coverage,gc, size=0.1, color=parse_lin), alpha=1.0, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  #geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+                             #xmax=cov_window[2], ymax=gc_window[2]), fill = "green", color="black", alpha=0.2, inherit.aes = FALSE) +
+  theme_bw()
 
-plot(unclass$coverage,unclass$gc,cex=0.25,col=unclass$color,pch=19,xlab="Coverage",ylab="GC content",xlim=xlim)
-legend("topright",leg[,1],col=leg[,2],pch=c(19,19,19,22),pt.bg=rect_color,pt.cex=2,cex=0.9)
+## annotated points with window
+ggplot(plt, aes(coverage, gc, color=hitlin_level, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  scale_colour_manual(values=cbPalette) +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+  ) +
+  geom_point(data=unclass, aes(coverage,gc, size=0.1), alpha=1.0, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+                             xmax=cov_window[2], ymax=gc_window[2]), fill = "green", color="black", alpha=0.2, inherit.aes = FALSE) +
+  theme_bw()
 
+## annotated points and unlclass with window and color_changing_points
+unclass[((unclass$coverage>=cov_window[1] & unclass$coverage<=cov_window[2]) & 
+          (unclass$gc>=gc_window[1] & unclass$gc<=gc_window[2])),]$color<-"blue"
+ggplot(plt, aes(coverage, gc, color=hitlin_level, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  scale_colour_manual(values=cbPalette) +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+  ) +
+  geom_point(data=unclass, aes(coverage, gc, size=0.1), color=unclass$color, alpha=1.0, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+                             xmax=cov_window[2], ymax=gc_window[2]), fill = "green", alpha=0.2, inherit.aes = FALSE) +
+  #xlim(min(unclass$coverage)*1.10, max(unclass$coverage)*1.10) +
+  #ylim(min(unclass$gc)*0.9, max(unclass$gc)*1.10) +
+  theme_bw()
+
+## Specified Target/Nontarget instead of taxonomy
+plt_tnt = plt
+ggplot(plt_tnt, aes(coverage, gc, color=parse_lin, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  scale_colour_manual(values=tntPalette) +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+  ) +
+  #geom_point(data=unclass, aes(log(coverage),gc, size=0.1), alpha=0.10, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  #geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+  #xmax=cov_window[2], ymax=gc_window[2]), fill = "green", color="black", alpha=0.2, inherit.aes = FALSE) +
+  theme_bw()
+
+## Specified Target/Nontarget with window
+ggplot(plt_tnt, aes(coverage, gc, color=parse_lin, size=log_evalue/-150)) + 
+  geom_point(alpha=0.20) +
+  xlab("log(Coverage)")+
+  ylab("GC content") +
+  scale_colour_manual(values=tntPalette) +
+  guides(
+    color = guide_legend(override.aes = list(alpha=1), title="Hit Taxonomy"), 
+    size = guide_legend(override.aes=list(alpha=1), title="Scaled Hit Significance")
+  ) +
+  #geom_point(data=unclass, aes(log(coverage),gc, size=0.1), alpha=0.10, inherit.aes = FALSE) +
+  guides (
+    alpha = FALSE
+  ) +
+  geom_rect(data=window, aes(xmin=cov_window[1], ymin=gc_window[1], 
+    xmax=cov_window[2], ymax=gc_window[2]), fill = "green", color="black", alpha=0.2, inherit.aes = FALSE) +
+  theme_bw()
 dev.off()
+ 
