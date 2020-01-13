dd50aeebf5548a49905370ebecaa614c6ee77ead
amsesk <amsesk@umich.edu>
2020-01-13 17:50:03 -0500

    start refactoring to module

--- scgid/update_scgid.py
+++ scgid/update_scgid.py
@@ -2,7 +2,17 @@ import subprocess
 import os
 import inspect
 import sys
+from scgid.module import Module
+from scgid.modcomm import LoggingEntity, ErrorHandler, Head
 
+class SCGIDUpdate(Module, LoggingEntity, ErrorHandler, Head):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+'''
 ## some variables
 scgid_bin = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 scgid_home = os.path.dirname(scgid_bin)
@@ -35,6 +45,7 @@ subprocess.call(reset)
 with open(os.path.join(scgid_bin, 'settings.py.sav')) as old, open(os.path.join(scgid_bin, 'settings.py')) as new:
     new_names = [x.split("=")[0] for x  in new.readlines()]
     old_names = [x.split("=")[0] for x in old.readlines()]
-    print old_names
-    print new_names
+    print (old_names)
+    print (new_names)
 os.rename(os.path.join(scgid_bin,'settings.py.sav'), os.path.join(scgid_bin,'settings.py'))
+'''
