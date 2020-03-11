43126fcf838bf419c2a756a6409aec196b033959
amsesk <amsesk@umich.edu>
2020-03-11 12:13:47 -0400

    complete infotable tests

--- tests/test_infotable.py
+++ tests/test_infotable.py
@@ -1,6 +1,15 @@
 import io
 import pytest
 from scgid.infotable import InfoTable
+from scgid.sequence import DNASequenceCollection, DNASequence
+import pandas as pd
+
+nucl_fasta = {
+"contig_1": DNASequence("contig_1", "ATCGGGACTGGTAGATA"),
+"contig_2": DNASequence("contig_2", "CCGGGCCTAGATGGAGA"),
+"contig_5": DNASequence("contig_5", "CCCGGGTAGATAGGAAA"),
+}
+nucl = DNASequenceCollection().from_dict(nucl_fasta)
 
 infotable_ldict_initial = [{
 	"contig": "contig_1",
@@ -91,7 +100,7 @@ infotable_ldict_reload_multiprotein = [{
 	"desc": "A description of the protein hit.",
 	"evalue": 1e-12,
 	"lineage": ["Eukaryota", "Fungi", "Ascomycota", "Pezizomycetes", "Pezizales", "Pezizoaceae", "Peziza", "species"],
-	"pertinent_taxlxl": "Fungi",
+	"pertinent_taxlvl": "Fungi",
 	"parse_lineage": "target"
 	},
 	{
@@ -105,7 +114,7 @@ infotable_ldict_reload_multiprotein = [{
 	"desc": "A description of the protein hit.",
 	"evalue": 1e-22,
 	"lineage": ["Eukaryota", "Fungi", "Ascomycota", "Pezizomycetes", "Pezizales", "Pezizoaceae", "Peziza", "species"],
-	"pertinent_taxlxl": "Fungi",
+	"pertinent_taxlvl": "Fungi",
 	"parse_lineage": "target"
 	},
 	{
@@ -119,10 +128,24 @@ infotable_ldict_reload_multiprotein = [{
 	"desc": "A description of the protein hit.",
 	"evalue": 1e-22,
 	"lineage": ['Bacteria', 'Proteobacteria', 'Alphaproteobacteria', 'Rhizobiales', 'Rhizobiaceae', 'Sinorhizobium/Ensifer group', 'Sinorhizobium'],
-	"pertinent_taxlxl": "Proteobacteria",
+	"pertinent_taxlvl": "Proteobacteria",
 	"parse_lineage": "nontarget"
 	}]
 
+infotable_ldict_raw_parsed = [
+	{
+	'contig': 'NODE_6371', 
+	'gc': 0.5928352992573176, 
+	'coverage': 92.818721, 
+	'pid': 'g96', 
+	'length': 521, 
+	'sseqid': 'sp|P55504|Y4JD_SINFN', 
+	'sp_os': 'Sinorhizobium fredii (strain NBRC 101917 / NGR234)', 
+	'desc': 'Uncharacterized protein y4jD OS=Sinorhizobium fredii (strain NBRC 101917 / NGR234) OX=394 GN=NGR_a03120 PE=3 SV=1', 
+	'evalue': '1.21e-79', 
+	'lineage': '	Bacteria;      Proteobacteria; Alphaproteobacteria;Rhizobiales,some group;   Rhizobiaceae, a sub group;	Sinorhizobium/Ensifer group;Sinorhizobium'
+	}
+]
 
 initial_colnames = ["contig","gc","coverage","pid","length","sseqid","sp_os","desc","evalue"]
 reload_colnames = ["contig","gc","coverage","pid","length","sseqid","sp_os","desc","evalue","lineage","pertinent_taxlvl","parse_lineage"]
@@ -135,43 +158,110 @@ infotable_reload_tsv = io.StringIO(
 	)
 infotable_reload_tsv.seek(0)
 
+def test___init__():
+	it = InfoTable()
+	assert isinstance(it, InfoTable)
+	assert it.df.shape == (0, 12)
+	assert it.tar is None
+	assert it.ex is None
+	assert it.children == []
+	assert it.parent is None
 
-'''
-def test___init__(self, target_taxa = None, ident="HEAD"):
+	it = InfoTable(target_taxa = {"target": "Fungi", "exception": "Ascomycota"})
+	assert isinstance(it, InfoTable)
+	assert it.df.shape == (0, 12)
+	assert it.tar == "Fungi"
+	assert it.ex == "Ascomycota"
+	assert it.children == []
+	assert it.parent is None
+
+def test_spawn_child():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	nested = it.df[it.df.contig == "contig_2"]
+	child = it.spawn_child("child1", nested)
+	assert child.df.shape == (3, 12)
+	assert child.df.iloc[0,:].contig == "contig_2"
+	assert len(it.children) == 1
+	assert all([isinstance(x, InfoTable) for x in it.children])
+
+@pytest.mark.skip
+def test_iter_descendants():
 	pass
 
-def test_spawn_child(self, ident, nested_frame):
-	pass
+def test_target_filter():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	child = it.target_filter()
+	assert it.df.shape == (5, 12)
+	assert child.df.shape == (3, 12)
+	assert child.df.contig.to_list() == ["contig_1", "contig_2", "contig_2"]
 
-def test_iter_descendants(self, at_head=True):
-	pass
+def test_collect_unclassifieds():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	unclassifieds = it.collect_unclassifieds(nucl)
+	assert unclassifieds.df.shape == (1, 4)
+	assert unclassifieds.df.iloc[0,:].contig == "contig_5"
 
-def test_target_filter(self):
-	pass
+def test_tidy ():
+	it = InfoTable()
+	it.populate(infotable_ldict_raw_parsed)
+	it.tidy(taxlvl_idx = 1)
+	assert it.df.iloc[0,:].lineage == ["Bacteria", "Proteobacteria", "Alphaproteobacteria", "Rhizobiales_some group", "Rhizobiaceae_a sub group", "Sinorhizobium/Ensifer group","Sinorhizobium"]
+	assert it.df.iloc[0,:].pertinent_taxlvl == "Proteobacteria"
 
-def test_collect_unclassifieds(self, nucl):
-	pass
+def test_rfilter():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	window = (0.4, 0.47)
+	feature = "gc"
+	child = it.rfilter(feature, window)
+	assert it.df.shape == (5, 12)
+	assert child.df.shape == (3, 12)
+	assert child.df.gc.to_list() == [0.45, 0.45, 0.45]
 
-def test_tidy (self, taxlvl_idx):
-	pass
+def test_rfilter_inplace_from_parent ():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	window = (0.4, 0.47)
+	feature = "gc"
+	child = it.rfilter(feature, window)
+	child.rfilter_inplace_from_parent(feature = feature, window = (0.49, 0.60))
+	assert it.df.shape == (5, 12)
+	assert child.df.shape == (2, 12)
+	assert child.df.gc.to_list() == [0.5, 0.55]
 
-def test_rfilter(self, feature, window, child_label=""):
-	pass
+def test_reset_from_parent ():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	window = (0.4, 0.47)
+	feature = "gc"
+	child = it.rfilter(feature = feature, window = window)
+	assert child.df.shape == (3, 12)
+	child.reset_from_parent()
+	assert it.df.shape == (5,12)
+	assert child.df.shape == (5, 12)
 
-def test_rfilter_inplace_from_parent (self, feature, window):
-	pass
+def test_reset_from_parent_fail_no_parent ():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	with pytest.raises(AssertionError):
+		it.reset_from_parent()
 
-def test_reset_from_parent (self):
-	pass
-
-def test_tnt_population (self):
-	pass
-'''
+def test_tnt_population ():
+	it = InfoTable()
+	it.populate(infotable_ldict_reload_multiprotein)
+	tnt_pop = it.tnt_population()
+	assert (tnt_pop.target, tnt_pop.nontarget) == (3.0, 2.0)
 
 def test_summary_stats ():
 	it = InfoTable()
 	it.populate(infotable_ldict_reload)
-
+	gc_ss = it.summary_stats("gc")
+	cov_ss = it.summary_stats("coverage")
+	assert (gc_ss.mean, gc_ss.std, gc_ss.min, gc_ss.max) == (0.475, 0.024999999999999994, 0.45, 0.5)
+	assert (cov_ss.mean, cov_ss.std, cov_ss.min, cov_ss.max) == (23, 22.5, 0.5, 45.5)
 
 def test_populate():
 	it = InfoTable()
@@ -193,7 +283,6 @@ def test_clear_decisions():
 	it = InfoTable()
 	it.keep = ["list", "of", "contigs", "to", "keep"]
 	it.dump = ["list", "of", "contigs", "to", "dump"]
-
 	it.clear_decisions()
 	assert it.keep is None
 	assert it.dump is None
