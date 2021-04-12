012ed5f71d44ba500b868cef426afef9d613fe70
amsesk <amsesk@umich.edu>
2021-04-11 21:40:47 -0400

    toupper in gc content calc

--- scgid/sequence.py
+++ scgid/sequence.py
@@ -339,7 +339,7 @@ class DNASequence(object):
         self.gc = float()
         self.gcCount = 0
         for letter in self.string:
-            if letter.toupper() == "G" or letter.toupper() == "C":
+            if letter.to_upper() == "G" or letter.to_upper() == "C":
                 self.gcCount += 1
         self.gc = float(self.gcCount) / float(self.length)
 
